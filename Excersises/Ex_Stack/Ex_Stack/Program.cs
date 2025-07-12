8c28295a15911a42847fb7c36343a59e6127874c
volam1311 <vophuclam1311@gmail.com>
2025-07-13 09:38:34 +1000

    Doing Inheritance excersises

---
+++ Excersises/Ex_Stack/Ex_Stack/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Ex_Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+        }
+    }
+    public class Stack
+    {
+        private readonly List<object> _stack = new List<object>();
+        public void Push (object obj)
+        {
+            _stack.Add(obj);
+        }
+        public object Pop()
+        {
+            if (_stack.Count == 0)
+            {
+                throw new InvalidOperationException("There is no element inside the stack");
+            }
+            else
+            {
+                var top = _stack[_stack.Count - 1];
+                _stack.RemoveAt(_stack.Count - 1);
+                return top;
+            }
+        }
+    }
+}
